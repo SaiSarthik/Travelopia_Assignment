4c86225564b7fd02bbd51b1e2eb79ecc0a2b4fe7
Sarthik-sai <skorra@registrarcorp.com>
2023-06-03 00:59:16 +0530

    Started creating Main Page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Main_page from './Pages/Main_page'
+
+function App() {
+
+  return (
+    <>
+     <Main_page />
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Main_page.jsx
@@ -0,0 +1,7 @@
+const Main_page = () => {
+  return (
+    <div>Main page</div>
+  )
+}
+
+export default Main_page
